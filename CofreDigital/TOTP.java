a29d4a9109c793a57d8ee8ec10a5f26e6f2be430
Livia <livialutz385@gmail.com>
2025-04-28 22:15:58 -0300

    Classes TOTP + Base32

---
+++ CofreDigital/TOTP.java
@@ -0,0 +1,35 @@
+package CofreDigital;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+
+
+public class TOTP {
+    private byte [] key = null;
+    private long timeStepInSeconds = 30;
+    // Construtor da classe. Recebe a chave secreta em BASE32 e o intervalo
+    // de tempo a ser adotado (default = 30 segundos). Deve decodificar a
+    // chave secreta e armazenar em key. Em caso de erro, gera Exception.
+    public TOTP(String base32EncodedSecret, long timeStepInSeconds) throws Exception {
+    }
+    // Recebe o HASH HMAC-SHA1 e determina o código TOTP de 6 algarismos
+    // decimais, prefixado com zeros quando necessário.
+    private String getTOTPCodeFromHash(byte[] hash) {
+    }
+    // Recebe o contador e a chave secreta para produzir o hash HMAC-SHA1.
+    private byte[] HMAC_SHA1(byte[] counter, byte[] keyByteArray) {
+    }
+    // Recebe o intervalo de tempo e executa o algoritmo TOTP para produzir
+    // o código TOTP. Usa os métodos auxiliares getTOTPCodeFromHash e HMAC_SHA1.
+    private String TOTPCode(long timeInterval) {
+    }
+    // Método que é utilizado para solicitar a geração do código TOTP.
+    public String generateCode() {
+    }
+    // Método que é utilizado para validar um código TOTP (inputTOTP).
+    // Deve considerar um atraso ou adiantamento de 30 segundos no
+    // relógio da máquina que gerou o código TOTP.
+    public boolean validateCode(String inputTOTP) {
+    }
+}
