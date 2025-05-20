02be4575413c8903732baaeb5cd20d42bc032453
Ricardo Bastos Leta Vieira <47068518+ricleta@users.noreply.github.com>
2025-05-20 13:44:28 +0000

    Removed *.jar from .gitignore and updated compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,2 +1,11 @@
-cd ..
+cd ../Cofre/
 mvn clean package
+
+cd target/
+cp CofreDigital-1.0-SNAPSHOT-jar-with-dependencies.jar ../../
+
+cd ../../LogRead/
+mvn clean package
+
+cd target/
+cp LogReader-1.0-SNAPSHOT-jar-with-dependencies.jar ../../
